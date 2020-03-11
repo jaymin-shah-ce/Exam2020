6de2fb7f29473d4ef9ad68ea20da776035bde2ec
Jaymin Shah <jaymin.shah@rapidops.com>
2020-03-11 17:05:30 +0530

    js_assessment1

--- public/js/matrix.js
+++ public/js/matrix.js
@@ -4,6 +4,38 @@ function mapdata() {
   let edges = [];
 
   // Write your logic here...
+  let i,j;
+  let matrixSize=4;
+  for(i=0; i<matrixSize; i++)
+  {
+    for(j=0; j<matrixSize; j++)
+    {
+      nodes.push({"data": {"id":`N${i}-${j}`}});
+    }
+  }
+
+  for(i=0; i<matrixSize; i++)
+  {
+    for(j=0; j<matrixSize; j++)
+    {
+      if(i+1<matrixSize)
+      {
+        edges.push({"data": {"id": `${i}-${j}S`, "source": `N${i}-${j}`, "target": `N${i+1}-${j}`}});
+        if(j+1<matrixSize)
+        {
+          edges.push({"data": {"id": `${i}-${j}SE`, "source": `N${i}-${j}`, "target": `N${i+1}-${j+1}`}});
+        }
+        if(j-1>=0)
+        {
+          edges.push({"data": {"id": `${i}-${j}SW`, "source": `N${i}-${j}`, "target": `N${i+1}-${j-1}`}});
+        }
+      }
+      if(j+1<matrixSize)
+      {
+        edges.push({"data": {"id": `${i}-${j}E`, "source": `N${i}-${j}`, "target": `N${i}-${j+1}`}});
+      }
+    }
+  }
 
   elements = {
     nodes,
